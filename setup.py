31c5883b9d5463613475d21e7aaa2de70f932457
Rajan Devkota <r.devkota.98@gmail.com>
2023-05-05 09:06:47 +0530

    setup file is created

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_package, setup
+
+def get_requirements():
+    pass
+
+setup
+{
+    name = 'Thyroid',
+    version = '0.0.1',
+    author = 'Rajan',
+    author_email = 'r.devkota.98@gmail.com',
+    package = find_package(),
+    install_requires = get_requirements(),
+}
+
+
