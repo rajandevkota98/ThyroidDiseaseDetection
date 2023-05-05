e1e941fd262d95287439bdca44cf5ce95fd38132
Rajan Devkota <r.devkota.98@gmail.com>
2023-05-05 12:57:07 +0530

    logging is created

---
+++ thyroid/logger.py
@@ -0,0 +1,20 @@
+import logging
+import os
+from datetime import datetime
+
+
+LOG_FILE_NAME = f"{datetime.now().strftime('a%m%d%Y__%H%M%S')}.log"
+LOG_FILE_DIR = os.path.join(os.getcwd(), 'logs')
+
+
+#create log folder if not available
+os.makedirs(LOG_FILE_DIR, exist_ok = True)
+
+#log file path
+LOG_FILE_PATH = os.path.join(LOG_FILE_DIR,LOG_FILE_NAME)
+
+logging.basicCongfig(
+    filename = LOG_FILE_PATH,
+    format = "[ %(asctime)s ] %(lineno)d  %(name)s  -  %(levelname)s  -%(messages)s",
+    level = logging.INFO,
+)
