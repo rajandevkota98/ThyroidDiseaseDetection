4496437d54db222f78874c0b9424ee96d072b390
Rajan Devkota <r.devkota.98@gmail.com>
2023-05-05 09:41:44 +0530

    setup is finished

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
-from setuptools import find_package, setup
+from setuptools import find_packages, setup
+from typing import List
 
-def get_requirements():
-    pass
+REQUIRED_FILE_NAME = 'requirements.txt'
+def get_requirements()->List[str]:
+    with open(REQUIRED_FILE_NAME) as file:
+        required_list = file.readlines()
+    required_list = [requirement_name.replace('\n', '') for requirement_name in required_list]
 
-setup
-{
+    if '-e .' in required_list:
+        required_list.remove('-e .')
+    return required_list
+
+setup(
     name = 'Thyroid',
     version = '0.0.1',
     author = 'Rajan',
     author_email = 'r.devkota.98@gmail.com',
-    package = find_package(),
+    package = find_packages(),
     install_requires = get_requirements(),
-}
+)
 
 
