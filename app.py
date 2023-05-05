d03b77335ac63406be3ac69858954d64aac125fb
Rajan Devkota <r.devkota.98@gmail.com>
2023-05-05 17:33:10 +0530

    sensor and logger debuged

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask
+import os,sys
+from thyroid.logger import logging
+from thyroid.exception import ThyroidException
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
+if __name__ =="__main__":
+    try:
+        test_logger_and_exception()
+    except Exception as e:
+        print(e)
+    
--- thyroid/logger.py
+++ thyroid/logger.py
@@ -13,8 +13,8 @@ os.makedirs(LOG_FILE_DIR, exist_ok = True)
 #log file path
 LOG_FILE_PATH = os.path.join(LOG_FILE_DIR,LOG_FILE_NAME)
 
-logging.basicCongfig(
+logging.basicConfig(
     filename = LOG_FILE_PATH,
-    format = "[ %(asctime)s ] %(lineno)d  %(name)s  -  %(levelname)s  -%(messages)s",
+        format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level = logging.INFO,
 )
